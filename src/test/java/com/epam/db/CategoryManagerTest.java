5eb38e4868312dd6eb10254df6df8e8b4b0d6552
EvgeniyaSirenko <sirenko.jane@gmail.com>
2021-07-06 17:44:23 +0300

    CategoryManagerTest class created but not realized yet

--- src/test/java/com/epam/db/CategoryManagerTest.java
+++ src/test/java/com/epam/db/CategoryManagerTest.java
@@ -1,6 +1,5 @@
 package com.epam.db;
 
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import com.epam.db.entity.Category;
 
@@ -12,9 +11,6 @@ public class CategoryManagerTest {
 	void createCategory() {
 		Category testCategory = new Category();
 		testCategory.setName("testName");
-		// Category createdCategory = CategoryManager.createCategory(testCategory);
-		assertNotNull(testCategory.getId());
-		// assertEquals(testCategory.getName(), createdCategory.getName());
 	}
 
 }
